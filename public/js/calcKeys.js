333e21f3cc71eecffc8401066611153383d9024e
Zezombye <zezombye>
2022-01-20 08:16:58 +0100

    some cleanup

---
+++ public/js/calcKeys.js
@@ -0,0 +1,33 @@
+const calcKeys = [
+    [
+        {calcCode: 79, displayName: "F1"},
+        {calcCode: 69, displayName: "F2"},
+        {calcCode: 59, displayName: "F3"},
+        {calcCode: 49, displayName: "F4"},
+        {calcCode: 39, displayName: "F5"},
+        {calcCode: 29, displayName: "F6"},
+        {calcCode: 38, displayName: "←"},
+        {calcCode: 28, displayName: "↑"},
+        {calcCode: 37, displayName: "↓"},
+        {calcCode: 27, displayName: "→"},
+    ],[
+        {calcCode: 71, displayName: "0"},
+        {calcCode: 72, displayName: "1"},
+        {calcCode: 62, displayName: "2"},
+        {calcCode: 52, displayName: "3"},
+        {calcCode: 73, displayName: "4"},
+        {calcCode: 63, displayName: "5"},
+        {calcCode: 53, displayName: "6"},
+        {calcCode: 74, displayName: "7"},
+        {calcCode: 64, displayName: "8"},
+        {calcCode: 54, displayName: "9"},
+    ],[
+        {calcCode: 61, displayName: "."},
+        {calcCode: 42, displayName: "+"},
+        {calcCode: 32, displayName: "-"},
+        {calcCode: 43, displayName: "×"},
+        {calcCode: 33, displayName: "÷"},
+        {calcCode: 44, displayName: "DEL"},
+        {calcCode: 31, displayName: "Exe"},
+    ]
+]
